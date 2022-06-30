f1ca4f5ee641b22e2b6ed637f6f6c04d5a5c570b
Kafilat <kafilatadewumi61@gmail.com>
2022-06-30 17:38:15 +0100

    update

--- 30DaysOfScript8/variable.js
+++ 30DaysOfScript8/variable.js
@@ -6,6 +6,7 @@ const dog ={}
 console.log(dog)
 
 // Add name, legs, color, age and bark properties for the dog object. The bark property is a method which return woof woof
+
 dog.name = 'Brownie',
 dog.legs = 2,
 dog.color = 'Brown',
@@ -17,6 +18,64 @@ dog.getBark = function (){
 }
 console.log(dog)
 
+const values = Object.values(dog)
+console.log(values)
 
+// Set new properties the dog object: breed, getDogInfo
+dog.breed = 'getDogInfo'
 
+// const users = {
+//         Alex: {
+//           email: 'alex@alex.com',
+//           skills: ['HTML', 'CSS', 'JavaScript'],
+//           age: 20,
+//           isLoggedIn: false,
+//           points: 30
+//         },
+//         Asab: {
+//           email: 'asab@asab.com',
+//           skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
+//           age: 25,
+//           isLoggedIn: false,
+//           points: 50
+//         },
+//         Brook: {
+//           email: 'daniel@daniel.com',
+//           skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+//           age: 30,
+//           isLoggedIn: true,
+//           points: 50
+//         },
+//         Daniel: {
+//           email: 'daniel@alex.com',
+//           skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+//           age: 20,
+//           isLoggedIn: false,
+//           points: 40
+//         },
+//         John: {
+//           email: 'john@john.com',
+//           skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+//           age: 20,
+//           isLoggedIn: true,
+//           points: 50
+//         },
+//         Thomas: {
+//           email: 'thomas@thomas.com',
+//           skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+//           age: 20,
+//           isLoggedIn: false,
+//           points: 40
+//         },
+//         Paul: {
+//           email: 'paul@paul.com',
+//           skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
+//           age: 20,
+//           isLoggedIn: false,
+//           points: 40
+//         }
+//       }
 
+// Find the person who has many skills in the users object.
+const result = Object.entries(users).sort((a, b) => a[1].skills.length - b[1].skills.length).pop()
+console.log(result())
