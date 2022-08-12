ff0de626ef3c1149191b5d4d802410efaa22d115
Kafilat <kafilatadewumi61@gmail.com>
2022-08-12 18:00:33 +0100

    30DaysOfAsabeneh

--- 30DaysOfScript23_Project1/variable.js
+++ 30DaysOfScript23_Project1/variable.js
@@ -2,6 +2,7 @@ let wrapper = document.querySelector('.wrapper')
 wrapper.style.display = 'flex'
 wrapper.style.alignItems = 'center'
 wrapper.style.flexDirection = 'column'
+wrapper.style.fontFamily = 'Sans-serif'
 
 
 
@@ -14,11 +15,13 @@ h1.style.color = '#5bbc7a'
 let dom = document.querySelector('.dom')
 dom.style.fontSize = '18px'
 dom.style.fontWeight = 100
+dom.style.margin = 0
 
 // let name = document.querySelector('.name')
 
 let number = document.querySelector('.number')
 number.style.color ='red'
+number.style.marginBottom = 0
 
 let input = document.querySelector('input')
 input.style.padding = '11px 15rem 11px 4px'
@@ -39,8 +42,9 @@ flex.style.display = 'flex'
 let container = document.querySelector('.container')
 container.style.display = 'grid'
 container.style.gridTemplateColumns = 'repeat(9, 1fr)'
-container.style.padding = '20px'
-container.style.width = '50%'
+container.style.marginTop = '20px'
+container.style.width = '100%'
+// container.style.paddingRight = '50px'
 
 
 
@@ -68,13 +72,14 @@ const generateNum= (max=51) => {
         const eachDiv = document.createElement('div')
         eachDiv.textContent = num
         eachDiv.style.width = '70px'
-        eachDiv.style.height = '30px'
+        eachDiv.style.height = '50px'
         eachDiv.style.display = 'flex'
         eachDiv.style.justifyContent = 'center'
         eachDiv.style.alignItems = 'center'
-        eachDiv.style.background = isPrime(num) ?'red' : num % 2 ? 'green':'yellow'
+        eachDiv.style.background = isPrime(num) ?'#fd5e53' : num % 2 ? '#21bf73':'#fddb3a'
         eachDiv.style.margin= '2px'
-
+        eachDiv.style.color = 'white'
+        eachDiv.style.fontSize = '30px'
 
         container.appendChild(eachDiv)
     })
