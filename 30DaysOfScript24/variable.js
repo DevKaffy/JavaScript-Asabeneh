ff08af4aa37f854172c5eb3d2c8da28bb51e97e1
Kafilat <kafilatadewumi61@gmail.com>
2022-08-29 17:28:55 +0100

    30DaysOfAsabeneh

--- 30DaysOfScript24/variable.js
+++ 30DaysOfScript24/variable.js
@@ -2,18 +2,25 @@ const body = document.body
 body.style.display = 'flex'
 body.style.alignItems ='center'
 body.style.flexDirection = 'column'
-body.style.backgroundImage = 'url(./img/galaxy.gif)'
-body.style.fontFamily= 'sanSerif'
+body.style.backgroundImage = 'url(./images/galaxy.gif)'
+body.style.fontFamily= 'sans-serif'
 
 const h1 = document.querySelector('h1')
 h1.style.color = 'white'
+
 
 const form = document.querySelector('form')
 // form.style.display ='flex'
 // form.style.alignItems = 'center'
 
 const input = document.querySelector('input')
+input.style.padding = '8px'
 
 const button = document.querySelector('button')
-button.style.padding = '15px'
+button.style.padding = '8px'
 // button.style.height = '6rem'
+
+const selectedId = getElementById('selectedid')
+const selectedPlanet = ['MERCURY', 'VENUS', 'EARTH', 'MOON', 'MARS', 'JUPITER', 'SATURN', 'URANUS', 'NEPTUNE', 'PLUTO']
+
+
