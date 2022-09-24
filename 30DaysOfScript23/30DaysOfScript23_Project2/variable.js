0826bc0e6df4ef974d1feb6cb5b82e89cba4c7c0
Kafilat <kafilatadewumi61@gmail.com>
2022-09-24 22:22:39 +0100

    Update

--- 30DaysOfScript23/30DaysOfScript23_Project2/variable.js
+++ 30DaysOfScript23/30DaysOfScript23_Project2/variable.js
@@ -1,1 +1,15 @@
+document.body.style.display = 'flex';
+document.body.style.alignItems = 'center';
+document.body.style.justifyContent= 'center';
+document.body.style.paddingTop= '5rem';
+// document.body.style.backgroundColor = 'red';
 
+let wrapper =document.querySelector('.wrapper')
+wrapper.style.height = '3rem';
+wrapper.style.width = '18rem';
+wrapper.style.border = '1px solid black';
+wrapper.style.display = 'flex';
+wrapper.style.alignItems = 'center';
+wrapper.style.justifyContent = 'center';
+wrapper.style.borderRadius = '5px'
+wrapper.style.boxShadow = '1px 1px 1px 1px rgba(0 0 0 0.5)'
